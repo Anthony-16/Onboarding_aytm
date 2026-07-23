5184bedf145ca8783c886e1b63baeee62c507469
Anthony-16 <Anthonydelpezzo@protonmail.com>
2026-07-23 18:38:27 -0400

    add group functionality

--- ms_entra.py
+++ ms_entra.py
@@ -28,6 +28,7 @@ def check_update(endpoint, key, value):
         r = requests.get(endpoint,headers={'Authorization': 'Bearer ' + token['access_token']})
         
         if r.ok and r.json()[key] == value:
+            time.sleep(1)
             return True
         else:
             counter += 1
@@ -61,9 +62,6 @@ licenses = {
         "removeLicenses": []
         }
 
-group = "Fill This In"
-
-
 print("Creaing Account")
 r = requests.post(endpoint,headers={'Authorization': 'Bearer ' + token['access_token']}, json=account_params)    
 print(r.status_code)
@@ -81,5 +79,26 @@ if check_update(endpoint+user_id+"?$select=usageLocation", "usageLocation", "US"
     r = requests.post(endpoint+user_id+"/assignLicense",headers={'Authorization': 'Bearer ' + token['access_token']}, json=licenses)
     print(r.status_code)
     print(r.text)
+
+group = {
+  "@odata.id": "https://graph.microsoft.com/v1.0/directoryObjects/"+user_id
+}
+laptop = "windows"
+
+r = requests.post('https://graph.microsoft.com/v1.0/groups/6c2bfbec-a0ee-4846-b15f-570fce5daa5b/members/$ref',headers={'Authorization': 'Bearer ' + token['access_token']}, json=group)
+print(r.status_code)
+print(r.text)
+
+if laptop == "mac":
+    r = requests.post('https://graph.microsoft.com/v1.0/groups/2caa48fe-ed5b-44f1-932e-4c2eac735b4b/members/$ref',headers={'Authorization': 'Bearer ' + token['access_token']}, json=group)
+    print(r.status_code)
+    print(r.text)
+
+elif laptop == "windows":
+    r = requests.post('https://graph.microsoft.com/v1.0/groups/093942c4-d55e-4a00-b35c-753b7df1fbe9/members/$ref',headers={'Authorization': 'Bearer ' + token['access_token']}, json=group)
+    print(r.status_code)
+    print(r.text)
+
+
 
 
