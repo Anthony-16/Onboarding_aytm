8ae36b505154b4bcc3b6c52e08f5cf2cbb5d3f13
Anthony-16 <Anthonydelpezzo@protonmail.com>
2026-07-27 14:02:15 -0400

    cleaned up code

--- ms_entra.py
+++ ms_entra.py
@@ -6,37 +6,13 @@ from ms_token import acquire_token
 
 token = acquire_token()
 endpoint = 'https://graph.microsoft.com/v1.0/users/'
+laptop = "windows"
 
 if "access_token" not in token:
     sys.exit()
 
-#Searching functionality (implement later)
-#r = requests.get(endpoint,headers={'Authorization': 'Bearer ' + token['access_token']})
-#data = r.json()
-# 
-#def find(user_input):
-#    for employee in data['value']:
-#        if employee['mail'] and (employee['mail'].strip().lower() == user_input.strip().lower()):
-#            print("User '"+user_input+"' found")
-#            found = True
-#            return
+auth = {'Authorization': 'Bearer ' + token['access_token']}
 
-def check_update(endpoint, key, value):
-    print("\nWait up to 1 minute while user data updates\n")
-    counter = 0
-    while counter <= 10:
-        r = requests.get(endpoint,headers={'Authorization': 'Bearer ' + token['access_token']})
-        
-        if r.ok and r.json()[key] == value:
-            time.sleep(1)
-            return True
-        else:
-            counter += 1
-            time.sleep(6)
-
-    return False
-    
-    
 account_params = {
         "accountEnabled": True,
         "displayName": "Test User",
@@ -62,42 +38,98 @@ licenses = {
         "removeLicenses": []
         }
 
-print("Creaing Account")
-r = requests.post(endpoint,headers={'Authorization': 'Bearer ' + token['access_token']}, json=account_params)    
-print(r.status_code)
-print(r.text) 
-user_id = r.json()['id']
+#Searching functionality (implement later)
+#r = requests.get(endpoint,headers={'Authorization': 'Bearer ' + token['access_token']})
+#data = r.json()
+# 
+#def find(user_input):
+#    for employee in data['value']:
+#        if employee['mail'] and (employee['mail'].strip().lower() == user_input.strip().lower()):
+#            print("User '"+user_input+"' found")
+#            found = True
+#            return
 
-if check_update(endpoint+user_id, "id", user_id) == True:
-    print("Adding usage location")
-    r = requests.patch(endpoint+user_id,headers={'Authorization': 'Bearer ' + token['access_token']}, json=additional_info)
-    print(r.status_code)
-    print(r.text)
+def check_update(endpoint, key, value):
+    print("\nWait up to 1 minute while user data updates\n")
+    counter = 0
+    while counter <= 10:
+        r = requests.get(endpoint,headers=auth)
+        
+        if r.ok and r.json()[key] == value:
+            time.sleep(6)
+            return True
+        else:
+            counter += 1
+            time.sleep(6)
 
-if check_update(endpoint+user_id+"?$select=usageLocation", "usageLocation", "US") == True:
-    print("Adding Licenses")
-    r = requests.post(endpoint+user_id+"/assignLicense",headers={'Authorization': 'Bearer ' + token['access_token']}, json=licenses)
-    print(r.status_code)
-    print(r.text)
+    return False
 
-group = {
-  "@odata.id": "https://graph.microsoft.com/v1.0/directoryObjects/"+user_id
-}
-laptop = "windows"
 
-r = requests.post('https://graph.microsoft.com/v1.0/groups/6c2bfbec-a0ee-4846-b15f-570fce5daa5b/members/$ref',headers={'Authorization': 'Bearer ' + token['access_token']}, json=group)
-print(r.status_code)
-print(r.text)
+def delete_account(user_id):
+    r = requests.delete(endpoint+user_id,headers=auth)
+    return r.status_code
 
-if laptop == "mac":
-    r = requests.post('https://graph.microsoft.com/v1.0/groups/2caa48fe-ed5b-44f1-932e-4c2eac735b4b/members/$ref',headers={'Authorization': 'Bearer ' + token['access_token']}, json=group)
-    print(r.status_code)
-    print(r.text)
 
-elif laptop == "windows":
-    r = requests.post('https://graph.microsoft.com/v1.0/groups/093942c4-d55e-4a00-b35c-753b7df1fbe9/members/$ref',headers={'Authorization': 'Bearer ' + token['access_token']}, json=group)
-    print(r.status_code)
-    print(r.text)
+def create_account():
+
+    #intial account creation
+    r = requests.post(endpoint,headers=auth, json=account_params)      
+    if r.ok:
+        user_id = r.json()['id']
+    else:
+        return r.status_code
+
+    #add usage location
+    if check_update(endpoint+user_id, "id", user_id) == True:
+        r = requests.patch(endpoint+user_id,headers=auth, json=additional_info)
+    else:
+        delete_account(user_id)
+        return r.status_code
+
+    #assign licenses
+    if check_update(endpoint+user_id+"?$select=usageLocation", "usageLocation", "US") == True:
+        r = requests.post(endpoint+user_id+"/assignLicense",headers=auth, json=licenses)
+    else:
+        delete_account(user_id)
+        return r.status_code
+
+    #assign group 
+    if r.ok:
+        group_url = "https://graph.microsoft.com/v1.0/groups/"
+        group = {
+            "@odata.id": "https://graph.microsoft.com/v1.0/directoryObjects/"+user_id
+        }
+        r = requests.post(group_url+'6c2bfbec-a0ee-4846-b15f-570fce5daa5b/members/$ref',headers=auth, json=group)
+    else:
+        delete_account(user_id)
+        return r.status_code
+
+    #assign os specific group
+    if r.ok:
+        if laptop == "mac":
+            r = requests.post(group_url+'2caa48fe-ed5b-44f1-932e-4c2eac735b4b/members/$ref',headers=auth, json=group)
+            if(r.ok):
+                return r.status_code
+            else:
+                delete_account(user_id)
+                return r.status_code
+        elif laptop == "windows":
+            r = requests.post(group_url+'093942c4-d55e-4a00-b35c-753b7df1fbe9/members/$ref',headers=auth, json=group)
+            if(r.ok):
+                return r.status_code
+            else:
+                delete_account(user_id)
+                return r.status_code
+    else:
+        delete_account(user_id)
+        return r.status_code
+    
+
+
+code = create_account()
+print(code)
+
+
 
 
 
